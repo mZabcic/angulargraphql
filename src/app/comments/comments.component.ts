45afd681032fd8e05a8fbf85c700ebad5f35fa09
mZabcic <mislav.zabcic@gmail.com>
2019-01-17 12:33:57 +0100

    Završeni komentari

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -16,38 +16,17 @@ export class CommentsComponent implements OnInit {
   @Input('postName') postName: string;
   comments : any[];
   respondComment : any = {}; 
-  newComment : string;
-  name : string;
+  newComment : string = "";
+  name : string = "";
+  count : number = 0;
+  error : boolean = false;
+  cError : boolean = false;
 
   constructor(private ngxService: NgxUiLoaderService, private apollo: Apollo, private _route: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
     this.ngxService.start();
-    const getComments = gql`{
-      comments(per_page : 100, orderby : "date", post: "192", parent: "0") {
-       id,
-       author_name,
-       date,
-    	 content
-    	children {
-      	id,
-      	author_name,
-      	date,
-      	content
-    	}
-  }
-} 
-   `;
-
-    this.apollo
-      .watchQuery({
-        query: getComments,
-        fetchPolicy: "network-only"
-      })
-      .valueChanges.pipe(map(((result: any) => result.data.comments))).subscribe(data => {
-        this.comments = data;
-        this.ngxService.stop();
-      })
+    this.getComments();
   }
 
   respond(comment) {
@@ -61,7 +40,102 @@ export class CommentsComponent implements OnInit {
   navigateTo(id) {
 
   }
+
+
+  comment() {
+    this.error = false;
+    if (this.newComment.length == 0) {
+      this.cError = true;
+    } else {
+    var parentId = this.respondComment.id == undefined ? 0 : this.respondComment.id;
+    const leaveComment = gql`mutation newComment {
+      newComment(post: ${this.post}, content:"${this.newComment}", parent : ${parentId}, author_name : "${this.name}") {
+        id,
+        content,
+        author_name,
+        date
+      }
+    }
+   `;
+   
+
+   this.apollo.mutate({
+    mutation: leaveComment
+  }).subscribe(({ data }) => {
+    this.name = "";
+    this.newComment = "";
+    this.getComments();
+  },(error) => {
+    this.error = true;
+  });
+}
+  }
  
 
+  getComments() {
+    const getComments = gql`{
+      comments(per_page : 100, orderby : "date", post: "${this.post}", parent: "0") {
+       id,
+       author_name,
+       date,
+    	 content
+    	children {
+      	id,
+      	author_name,
+      	date,
+        content,
+        children {
+          id,
+          author_name,
+          date,
+          content
+          children {
+            id,
+            author_name,
+            date,
+            content
+            children {
+              id,
+              author_name,
+              date,
+              content
+            }
+          }
+        }
+    	}
+  }
+} 
+   `;
 
+    this.apollo
+      .watchQuery({
+        query: getComments,
+        fetchPolicy: "network-only"
+      })
+      .valueChanges.pipe(map(((result: any) => result.data.comments))).subscribe(data => {
+        this.comments = data;
+        this.setCount(this.comments);
+        this.ngxService.stop();
+      })
+  }
+
+  setCount(comments : any[]) {
+    this.count = 0;
+    for (let comment of comments) {
+      this.count++;
+      for (let child of comment.children) {
+        this.count++;
+      }
+    }
+  }
+
+
+  check() {
+    if (!this.cError)
+      return;
+    else {
+      if (this.newComment.length > 0)
+        this.cError = false;
+    }
+  }
 }
